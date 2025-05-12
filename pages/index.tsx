1a25805fe1a2435640cbd6fbf09f5b0cbf6053c4
miwa reou <reou@miwanoMacBook-Pro.local>
2025-05-12 11:00:49 +0900

    雛形の猫をランダムで出すサイト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
 import { NextPage } from "next"
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-    return <div>ねこ画像予定地</div>
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading ] = useState(true);
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url)
+        setLoading(false);
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div>{loading || <img src={imageUrl}/> }</div>
+        </div>
+    );
 };
 export default IndexPage;
 
-const fetchImage = async () => {
+type Image = {
+    url:string;
+}
+
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log(images);
